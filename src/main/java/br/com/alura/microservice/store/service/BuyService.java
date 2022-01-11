c730b2e5b33e92f5d6c23d98ddb31d4911abd5d6
wtrindades <wtrindades@gmail.com>
2022-01-11 15:51:14 -0300

    refactor: change access to provider microservice by eureka

--- src/main/java/br/com/alura/microservice/store/service/BuyService.java
+++ src/main/java/br/com/alura/microservice/store/service/BuyService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,16 @@ import br.com.alura.microservice.store.controller.dto.InfoProviderDTO;
 
 @Service
 public class BuyService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void processBuy(BuyDTO buy) {
 		
-		RestTemplate client = new RestTemplate();
 		String state = buy.getAddress().getState();
 		
 		ResponseEntity<InfoProviderDTO> exchange = 
-				client.exchange("http://localhost:8081/info/" + state, HttpMethod.GET, null, InfoProviderDTO.class);
+				client.exchange("http://provider/info/" + state, HttpMethod.GET, null, InfoProviderDTO.class);
 		
 		System.out.println(exchange.getBody().getAddress());
 		
